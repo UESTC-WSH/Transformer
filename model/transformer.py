b3c0149f9d46e287561785f4dbf81b2e6509c3e0
master-shw <34528863+UESTC-WSH@users.noreply.github.com>
2022-06-06 11:04:55 +0800

    Add files via upload

---
+++ model/transformer.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from Transformer.model.submodel import *
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder().cuda()
+        self.decoder = Decoder().cuda()
+        self.projection = nn.Linear(d_model, tgt_vocab_size, bias=False)
+
+    def forward(self, enc_inputs, dec_inputs):
+        """
+        enc_inputs : [batch_size, src_len]
+        dec_inputs : [batch_size, tgt_len]
+        """
+        enc_outputs, enc_self_attns = self.encoder(enc_inputs)
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_inputs, enc_outputs)
+        dec_logits = self.projection(dec_outputs)
+        return dec_logits.view(-1, dec_logits.size(-1)), enc_self_attns, dec_self_attns, dec_enc_attns
+
